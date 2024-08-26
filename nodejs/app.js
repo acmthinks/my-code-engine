7c16d161209d2aba69ba64dc0388ecc3425acb8c
Andrea C. Crawford <acm@us.ibm.com>
2024-08-26 09:29:35 -0400

    changed name of app to my-nodejs-app

--- nodejs/app.js
+++ nodejs/app.js
@@ -6,7 +6,7 @@ var app = express();
 http.createServer(app);
 
 app.get('/', function (req, res) {
-  res.send('Hello World, from the aliens!');
+  res.send('Hello World');
 });
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
